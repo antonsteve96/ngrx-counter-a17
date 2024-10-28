27e262a15a6d22df5a8aab1848a5a41d9735f6c4
Stefano Antonetti <anton.steve96@libero.it>
2024-10-28 16:23:43 +0100

    Updated counter.selectors.ts

--- src/app/counter/state/counter.selectors.ts
+++ src/app/counter/state/counter.selectors.ts
@@ -1,13 +1,18 @@
-import {AppState, appStateKey} from "../../store/app.state";
-import {CounterState} from "./counter.state";
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AppState } from '../../store/app.state';
+import {COUNTER_STATE, CounterState} from './counter.state';
 
-export const getCounterState = (state: AppState): CounterState => {
-  // @ts-ignore
-  return state[appStateKey].counter;
-}
+// Definiamo il selettore per l'intero stato del contatore
+export const getCounterState = createFeatureSelector<CounterState>(COUNTER_STATE);
 
 // Selettore per ottenere il valore del contatore
-export const getCounter = (state: AppState): number => getCounterState(state).counter;
+export const selectCounter = createSelector(
+  getCounterState,
+  (state: CounterState) => state.counter
+);
 
 // Selettore per ottenere il nome del sito
-export const getSiteName = (state: AppState): string => getCounterState(state).siteName;
+export const getSiteName = createSelector(
+  getCounterState,
+  (state: CounterState) => state.siteName
+);
